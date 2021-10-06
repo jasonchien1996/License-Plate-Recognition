b365c03cfd60d7d5ed2a8e5719f88bf32baeaa78
jasonchien1996 <43934912+jasonchien1996@users.noreply.github.com>
2021-10-06 18:19:12 +0800

    add control blocks

--- alpr.cpp
+++ alpr.cpp
@@ -1,13 +1,15 @@
-/*
-nvcc alpr.cpp util.cpp car.cpp imgproc.cu -o alpr.out -std c++14 -gencode=arch=compute_72,code=sm_72 -I/usr/local/include/opencv4 -I/usr/local/include/onnxruntime -I/home/pomchi/Desktop/alpr/darknet/include -L/usr/local/lib -lopencv_core -lopencv_videoio -lopencv_imgcodecs -lopencv_imgproc -lopencv_dnn -lopencv_cudaimgproc -lopencv_highgui -lopencv_cudaarithm -lopencv_cudawarping -lopencv_cudafilters -ldarknet -lonnxruntime
-*/
-
 #include "car.hpp"
 #include "util.hpp"
 #include "imgproc.cuh"
 #include "darknet.h"
+#include <dirent.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <ctime>
 #include <string>
 #include <vector>
+#include <iostream>
+#include <fstream>
 #include <opencv2/opencv.hpp>
 #include <onnxruntime/core/session/onnxruntime_cxx_api.h>
 
@@ -21,8 +23,9 @@ nvcc alpr.cpp util.cpp car.cpp imgproc.cu -o alpr.out -std c++14 -gencode=arch=c
 //for darknet
 #define OCR_TRESHOLD .3f
 
-#define CONSEC_CORRECT 5
-#define MAX_FRAME 25
+#define VIDEO_PATH "./video/video (3).mp4"
+#define CONSEC_CORRECT 6
+#define MAX_IDLE 6
 
 using namespace cv;
 using namespace std;
@@ -35,11 +38,11 @@ char OCR_DATASET[] = "ocr/ocr-net.data";
 
 template <typename T>
 ostream& operator<<(ostream& os, const vector<T>& v);
-ostream& operator<<(ostream& os, const ONNXTensorElementDataType& type);
+ostream& operator<<(ostream&, const ONNXTensorElementDataType&);
 bool stop(int, string&);
 
 int main() {
-	VideoCapture capture( "./video/video (3).mp4" );
+	VideoCapture capture( VIDEO_PATH );
 	if( !capture.isOpened() ) {
 		cout << "Unable to open file!";
 		return 0;
@@ -108,15 +111,44 @@ int main() {
 	network *ocr_net = load_network( OCR_NETCFG, OCR_WEIGHTS, 0 );
 	metadata ocr_meta = get_metadata( OCR_DATASET );
 	set_batch_network(ocr_net, 1);
+	
+	int imgCount = 0;           //number of images of the current vehicle
+    int vehicleCount = 1;       //number of vehicles
+    int idle = 0;               //consecutive images with no plate
+    bool skip = false;          //set to true if early stoppping
+
+	string date = getDate();
+	if(opendir(date.c_str()) == nullptr) {
+		cout << "create directory " << date << endl;
+		mkdir(date.c_str(), 0775);
+	}
+	fstream record;
+    record.open( "./" + date + "/" + date + ".txt", ios::out);
+	string crop_dir_path = "./" + date + "/" + to_string(vehicleCount);			
 
 	Car plate;
 	vector<Mat> images;
 	while(1) {
-		auto Start = high_resolution_clock::now();
+		auto t1 = high_resolution_clock::now();
+		
+		auto now = getDate().c_str();
+		if( now != date ) {
+			record.close();
+            date = now;            
+			if(opendir(date.c_str()) == nullptr) {
+				cout << "create directory " << date << endl;
+				mkdir(date.c_str(), 0775);
+			}
+            record.open( "./" + date + "/" + date + ".txt", ios::out);
+		}
+		crop_dir_path = "./" + date + "/" + to_string(vehicleCount);
 
 		Mat imageBGR, resizedImageBGR, resizedImageRGB, resizedImage, preprocessedImage;
-		if( !capture.read(imageBGR) ) continue;		
-		//imageBGR = cv::imread("./picture/1.jpg");
+		if( !capture.read(imageBGR) ) {
+			cout << "video ended or error\n";			
+			continue;
+		}
+		//imageBGR = imread("./picture/1.jpg");
 		
 		resize(imageBGR, resizedImageBGR, Size(416, 416), InterpolationFlags::INTER_CUBIC);
 		resizedImageBGR.convertTo(resizedImage, CV_32F, 1.0 / 255);
@@ -157,9 +189,22 @@ int main() {
 		}
 		
 		if(boxes.size() == 0) {
-			cout << "no plate found" << endl;
-			auto Stop = high_resolution_clock::now();
-			auto duration = duration_cast<microseconds>(Stop - Start);
+			idle += 1;
+			if( idle == MAX_IDLE ) {
+				if( skip == false ) {
+                    record << crop_dir_path << endl;
+                    record << "number of images scanned: " << imgCount << endl;
+                    record << "predicted category: " << plate.getDominantCategory() << endl;
+                    record << "predicted result: " << plate.vote() << endl << endl;
+				}
+				skip = false;
+				imgCount = 0;
+				vehicleCount += 1;
+			}
+
+			cout << "idle\n";
+			auto t2 = high_resolution_clock::now();
+			auto duration = duration_cast<microseconds>(t2 - t1);
 			printf("%4.3fseconds\n", duration.count()*0.000001);
 			cout << "=================================="<< endl;
 			continue;
@@ -168,14 +213,21 @@ int main() {
 		nms(boxes, iou_threshold);
 		
 		if(boxes.size() > 1) {
-			cout << boxes.size() << "plates found" << endl;
-			auto Stop = high_resolution_clock::now();
-			auto duration = duration_cast<microseconds>(Stop - Start);
+			cout << "too many plates found" << endl;
+			auto t2 = high_resolution_clock::now();
+			auto duration = duration_cast<microseconds>(t2 - t1);
 			printf("%4.3fseconds\n", duration.count()*0.000001);
 			cout << "=================================="<< endl;
 			continue;
 		}
 		
+		if( skip ) {
+			cout << "skip\n";
+			continue;
+		}
+		imgCount += 1;
+		idle = 0;
+
 		util::box &bb = boxes[0];
 		int x = int(bb.tl[0]*imageBGR.cols);
 		int y = int(bb.tl[1]*imageBGR.rows);
@@ -198,15 +250,31 @@ int main() {
 		if(croppedBGR.cols > 416) resize(croppedBGR, croppedBGR, Size(416, 208), InterpolationFlags::INTER_CUBIC);
 		//imshow("croppedBGR", croppedBGR);
 		//waitKey(0);
+		
+		if( imgCount%50 == 1){
+			auto path = crop_dir_path.c_str();
+			auto dest = crop_dir_path + "/" + to_string(imgCount) + ".png";
+			if(opendir(path) == nullptr) {
+				cout << "create directory " << path << endl;
+				mkdir(path, 0775);		
+			}
+			cv::imwrite(dest, croppedBGR);
+		}
 
 		Mat a,b;//create dummy Mat for space
 		images.push_back(a);
 		images.push_back(b);
 		static int num_frame = 0;
+		num_frame += 1;
 		if( process(croppedBGR, &images) ) {
 			for( cv::Mat &img : images) {		
 				//cv::imshow("img", img);
 				//cv::waitKey(0);
+				if( skip ) {
+					cout << "skip\n";
+					break;
+				}
+
 				image im = mat_to_image(img);
 				
 				string OCRresult;
@@ -215,41 +283,39 @@ int main() {
 				
 				if( OCRresult.length() < 6 || OCRresult.length() > 7 ) {
 					cout << "ocr: " << OCRresult << endl;
-					auto Stop = high_resolution_clock::now();
-					auto duration = duration_cast<microseconds>(Stop - Start);
-					printf("%4.3fseconds\n", duration.count()*0.000001);
-					cout << "=================================="<< endl;
+					cout << "category: " << endl;
+					cout << "confidence: " << endl;
 					continue;
 				}
 				int category = -1;
 
 				bool imply_category1 = plate.recognize( OCRresult, category );
+
 				if ( OCRresult == "") {
-					cout << "ocr: " << OCRresult << endl;
-					auto Stop = high_resolution_clock::now();
-					auto duration = duration_cast<microseconds>(Stop - Start);
-					printf("%4.3fseconds\n", duration.count()*0.000001);
-					cout << "=================================="<< endl;
+					cout << "ocr: " << endl;
+					cout << "category: " << endl;
+					cout << "confidence: " << endl;
 					continue;
 				}
-
 				cout << "ocr: " << OCRresult << endl;
 				cout << "category: " << category << endl;
-				cout << "confidence: " << conf << endl;	
-				//cout << "imply_category1: " << imply_category1 << endl;
+				cout << "confidence: " << conf << endl;
 				
 				plate.setCounter( OCRresult, category, conf, imply_category1 );
-				num_frame += 1;
 
-				if( stop(category, OCRresult) ) return 0;
-				else if (num_frame == MAX_FRAME) {	
-					cout << "predicted category: " << plate.getDominantCategory() << endl;
-					cout << "predicted result:" << plate.vote() << endl;
-					return 0;
+				if( stop(category, OCRresult) ) {					
+					record << crop_dir_path << endl;
+					record << "" << endl;
+                    record << "number of images scanned: " << imgCount << endl;
+                    record << "predicted category: " << category << endl;
+                    record << "predicted result: " << OCRresult << endl << endl;
+					skip = true;	
+								
+					//return 0;
 				}
 			}
-			auto Stop = high_resolution_clock::now();
-			auto duration = duration_cast<microseconds>(Stop - Start);
+			auto t2 = high_resolution_clock::now();
+			auto duration = duration_cast<microseconds>(t2 - t1);
 			printf("%4.3fseconds\n", duration.count()*0.000001);
 			cout << "=================================="<< endl;		
 		}
@@ -288,15 +354,18 @@ bool stop(int category, string &result) {
 					}
 					if( consec7[i] < CONSEC_CORRECT ) Stop = false;
 				}
+				result[i] = currentSeven[i];
 			}
 
 			for( int i = 0; i < 7 ; ++i ) cout << currentSeven[i];
 			cout << endl;
 			for( int i = 0; i < 7 ; ++i ) cout << consec7[i];
 			cout << endl;
+			cout << "----------------------------------" << endl;
 
 			if(Stop) {
-				cout << "predicted result: ";
+				cout << "predicted result: \n";
+				cout << "----------------------------------" << endl;
 				for( int i = 0; i < 7 ; ++i ) cout << currentSeven[i];
 				cout << endl;
 			}
@@ -323,16 +392,19 @@ bool stop(int category, string &result) {
 					}
 					if( consec6[i] < CONSEC_CORRECT ) Stop = false;
 				}
+				result[i] = currentSix[i];
 			}
 
 			for( int i = 0; i < 6 ; ++i ) cout << currentSix[i];
 			cout << endl;
 			for( int i = 0; i < 6 ; ++i ) cout << consec6[i];
 			cout << endl;
+			cout << "----------------------------------" << endl;
 
 			if(Stop) {
 				cout << "predicted category: " << category << endl;
-				cout << "predicted result: ";
+				cout << "predicted result: \n";
+				cout << "----------------------------------" << endl;
 				for( int i = 0; i < 6 ; ++i ) cout << currentSix[i];
 				cout << endl;
 			}
@@ -360,16 +432,19 @@ bool stop(int category, string &result) {
 			}
 			for( int i = 0; i < 6 ; ++i ) {
 				if( consec6[i] < CONSEC_CORRECT ) Stop = false;
+				result[i] = currentSix[i];
 			}
 
 			for( int i = 0; i < 6 ; ++i ) cout << currentSix[i];
 			cout << endl;
 			for( int i = 0; i < 6 ; ++i ) cout << consec6[i];
 			cout << endl;
+			cout << "----------------------------------" << endl;
 
 			if(Stop) {
 				cout << "predicted category: " << category << endl;
-				cout << "predicted result: ";
+				cout << "predicted result: \n";
+				cout << "----------------------------------" << endl;
 				for( int i = 0; i < 6 ; ++i ) cout << currentSix[i];
 				cout << endl;
 			}
@@ -378,23 +453,29 @@ bool stop(int category, string &result) {
 			cout << "error in stop()!\n";
 			exit(1);
 	}
+	if(Stop) {
+		currentSeven = {'#', '#', '#', '#', '#', '#', '#'};
+		currentSix = {'#', '#', '#', '#', '#', '#'};
+		consec7 = {0, 0, 0, 0, 0, 0, 0};
+		consec6 = {0, 0, 0, 0, 0, 0};
+	}
 	return Stop;
 }
 
 template <typename T>
-std::ostream& operator<<(std::ostream& os, const std::vector<T>& v)
+ostream& operator<<(ostream& os, const vector<T>& v)
 {
-    os << "[";
-    for (int i = 0; i < v.size(); ++i)
-    {
-        os << v[i];
-        if (i != v.size() - 1)
-        {
-            os << ", ";
-        }
-    }
-    os << "]";
-    return os;
+	os << "[";
+	for (int i = 0; i < v.size(); ++i)
+	{
+	    os << v[i];
+	    if (i != v.size() - 1)
+	    {
+	        os << ", ";
+	    }
+	}
+	os << "]";
+	return os;
 }
 
 /**
@@ -404,65 +485,64 @@ std::ostream& operator<<(std::ostream& os, const std::vector<T>& v)
  * @param type
  * @return std::ostream&
  */
-std::ostream& operator<<(std::ostream& os, const ONNXTensorElementDataType& type)
+ostream& operator<<(ostream& os, const ONNXTensorElementDataType& type)
 {
-    switch (type)
-    {
-        case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_UNDEFINED:
-            os << "undefined";
-            break;
-        case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_FLOAT:
-            os << "float";
-            break;
-        case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_UINT8:
-            os << "uint8_t";
-            break;
-        case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_INT8:
-            os << "int8_t";
-            break;
-        case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_UINT16:
-            os << "uint16_t";
-            break;
-        case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_INT16:
-            os << "int16_t";
-            break;
-        case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_INT32:
-            os << "int32_t";
-            break;
-        case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_INT64:
-            os << "int64_t";
-            break;
-        case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_STRING:
-            os << "std::string";
-            break;
-        case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_BOOL:
-            os << "bool";
-            break;
-        case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_FLOAT16:
-            os << "float16";
-            break;
-        case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_DOUBLE:
-            os << "double";
-            break;
-        case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_UINT32:
-            os << "uint32_t";
-            break;
-        case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_UINT64:
-            os << "uint64_t";
-            break;
-        case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_COMPLEX64:
-            os << "float real + float imaginary";
-            break;
-        case ONNXTensorElementDataType::
-            ONNX_TENSOR_ELEMENT_DATA_TYPE_COMPLEX128:
-            os << "double real + float imaginary";
-            break;
-        case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_BFLOAT16:
-            os << "bfloat16";
-            break;
-        default:
-            break;
-    }
-
-    return os;
+	switch (type)
+	{
+	    case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_UNDEFINED:
+	        os << "undefined";
+	        break;
+	    case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_FLOAT:
+	        os << "float";
+	        break;
+	    case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_UINT8:
+	        os << "uint8_t";
+	        break;
+	    case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_INT8:
+	        os << "int8_t";
+	        break;
+	    case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_UINT16:
+	        os << "uint16_t";
+	        break;
+	    case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_INT16:
+	        os << "int16_t";
+	        break;
+	    case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_INT32:
+	        os << "int32_t";
+	        break;
+	    case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_INT64:
+	        os << "int64_t";
+	        break;
+	    case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_STRING:
+	        os << "std::string";
+	        break;
+	    case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_BOOL:
+	        os << "bool";
+	        break;
+	    case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_FLOAT16:
+	        os << "float16";
+	        break;
+	    case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_DOUBLE:
+	        os << "double";
+	        break;
+	    case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_UINT32:
+	        os << "uint32_t";
+	        break;
+	    case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_UINT64:
+	        os << "uint64_t";
+	        break;
+	    case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_COMPLEX64:
+	        os << "float real + float imaginary";
+	        break;
+	    case ONNXTensorElementDataType::
+	        ONNX_TENSOR_ELEMENT_DATA_TYPE_COMPLEX128:
+	        os << "double real + float imaginary";
+	        break;
+	    case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_BFLOAT16:
+	        os << "bfloat16";
+	        break;
+	    default:
+	        break;
+	}
+	return os;
 }
